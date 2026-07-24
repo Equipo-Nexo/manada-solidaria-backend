769041cc2c731b70b1aa2f50bcb73bdcd415ce6a
Fedesan14 <fede14.sanchez@gmail.com>
2026-07-24 19:27:14 -0300

    [REF][MS-113]: add imageId to user posts

--- src/main/java/com/nexo/manada_solidaria_backend/users/controllers/responses/UserPostResponse.java
+++ src/main/java/com/nexo/manada_solidaria_backend/users/controllers/responses/UserPostResponse.java
@@ -14,12 +14,14 @@ public abstract class UserPostResponse {
     private final UUID id;
     private final String title;
     private final long createdSince;
+    private final String imageId;
     private final String postType;
 
-    protected UserPostResponse(UUID id, String title, LocalDateTime createdAt, String postType) {
+    protected UserPostResponse(UUID id, String title, LocalDateTime createdAt, String imageId, String postType) {
         this.id = id;
         this.title = title;
         this.createdSince = ChronoUnit.DAYS.between(createdAt, LocalDateTime.now());
+        this.imageId = imageId;
         this.postType = postType;
     }
 }
