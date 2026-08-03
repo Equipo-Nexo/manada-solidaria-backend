a75b39fdc918a1d6affe5d8d50e8a7e3183e088d
Elian <elian.enria1@gmail.com>
2026-08-03 09:33:48 -0300

    [REF][MS-121]: remove geolocation endpoint

--- src/main/java/com/nexo/manada_solidaria_backend/geolocation/services/interfaces/GeolocationService.java
+++
@@ -1,7 +0,0 @@
-package com.nexo.manada_solidaria_backend.geolocation.services.interfaces;
-
-import com.nexo.manada_solidaria_backend.geolocation.controllers.responses.GeolocationResponse;
-
-public interface GeolocationService {
-    GeolocationResponse locate(String ip);
-}
