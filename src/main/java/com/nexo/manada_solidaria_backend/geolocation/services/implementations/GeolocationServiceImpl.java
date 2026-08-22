b0a5b24fbfdb64bafa07fbe4ead51dbbd591fac8
Fedesan14 <fede14.sanchez@gmail.com>
2026-08-22 17:05:31 -0300

    [REF][MS-139]: throw error on null geolocation

--- src/main/java/com/nexo/manada_solidaria_backend/geolocation/services/implementations/GeolocationServiceImpl.java
+++ src/main/java/com/nexo/manada_solidaria_backend/geolocation/services/implementations/GeolocationServiceImpl.java
@@ -8,9 +8,12 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +21,7 @@ import java.util.List;
 public class GeolocationServiceImpl implements GeolocationService {
 
     private final GeoapifyRestClient geoapifyRestClient;
+    public static final String UNKNOWN_PROPERTY = "unknown";
 
     @Override
     public List<GeolocationResponse> getGeolocations(String text, Integer limit, Double longitude, Double latitude) {
@@ -43,13 +47,14 @@ public class GeolocationServiceImpl implements GeolocationService {
                     .stream()
                     .findFirst()
                     .map(feature -> buildGeolocationResponse(feature.properties()))
-                    .orElse(null);
-        } catch (Exception e) {
+                    .orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "No se encontró una ubicación válida"));
+        } catch (RestClientException e) {
             throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, "El servicio de geolocalizacion no se encuentra disponible.");
         }
     }
 
     private GeolocationResponse buildGeolocationResponse(GeoapifyResponse.Property property) {
+        if (isUnknown(property)) return null;
         return new GeolocationResponse(
                 property.country(),
                 property.state(),
@@ -65,5 +70,9 @@ public class GeolocationServiceImpl implements GeolocationService {
                 property.address_line1(),
                 property.address_line2()
         );
+    }
+
+    private boolean isUnknown(GeoapifyResponse.Property property) {
+        return property.result_type().equals(UNKNOWN_PROPERTY);
     }
 }
