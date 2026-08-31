e7cf844a0e1bf67b793b5f69fe594ea2175391b8
LuciaRosales9 <luciarosalesb9@gmail.com>
2026-08-31 17:48:15 -0300

    [FTR][MS-156]: add logs

--- src/main/java/com/nexo/manada_solidaria_backend/campaigns/cronjobs/NewsCampaignFinalizationJob.java
+++ src/main/java/com/nexo/manada_solidaria_backend/campaigns/cronjobs/NewsCampaignFinalizationJob.java
@@ -2,9 +2,11 @@ package com.nexo.manada_solidaria_backend.campaigns.cronjobs;
 
 import com.nexo.manada_solidaria_backend.campaigns.services.interfaces.CampaignService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class NewsCampaignFinalizationJob {
@@ -13,6 +15,7 @@ public class NewsCampaignFinalizationJob {
 
     @Scheduled(cron = "0 */15 * * * *")
     public void finalizeExpiredNewsCampaigns() {
+        log.debug("Running scheduled job: finalizeExpiredNewsCampaigns");
         campaignService.finalizeExpiredNewsCampaigns();
     }
 }
