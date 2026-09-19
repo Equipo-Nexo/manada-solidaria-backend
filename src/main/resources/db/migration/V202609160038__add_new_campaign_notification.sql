a313cae4bd61b6679ebb556b422760f5fb9e2de3
fedesan14 <fede14.sanchez@gmail.com>
2026-09-19 18:08:58 -0300

    [REF][MS-173]: refactor due to code review

--- src/main/resources/db/migration/V202609160038__add_new_campaign_notification.sql
+++ src/main/resources/db/migration/V202609160038__add_new_campaign_notification.sql
@@ -12,7 +12,7 @@ VALUES
     UUID_TO_BIN('cb3daadc-960e-46a4-aa6b-cb133ec477b7'),
     '2026-09-06 20:52:13.551601',
     'Nueva colecta solidaria',
-    '{user} necesita tu ayuda, sumá to aporte por una causa benéfica.',
+    '{user} necesita tu ayuda, sumá tu aporte por una causa benéfica.',
     'NEW_FUNDRAISING_CAMPAIGN',
     NULL,
     '/colectas/{fundraisingId}'
